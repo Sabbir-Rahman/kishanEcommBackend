a871658364874531da3a2dd506b47ff553e57d21
Sabbir <sabbirrahman42@iut-dhaka.edu>
2021-07-05 11:27:16 +0600

    timeout

--- test/auth.test.js
+++ test/auth.test.js
@@ -5,7 +5,7 @@ const app = require('../app')
 var expect = chai.expect;
 
 describe('Auth api', function () {
-    this.timeout(20000)
+    this.timeout(10000)
 
     it('POST /auth/register --> create a user', () => { 
         return request(app)
@@ -20,12 +20,13 @@ describe('Auth api', function () {
         
     })
 
+    this.timeout(10000)
 
     it('POST /auth/login --> login of a user', () => { 
         return request(app)
         .post('/auth/login').send({
-            email: 'test@gmail.com',
-            password: 'password@1234'
+            email: 'sabbirrahman42@iut-dhaka.edu',
+            password: '123456'
         })
         .expect('Content-Type', /json/)
         .expect(200)
