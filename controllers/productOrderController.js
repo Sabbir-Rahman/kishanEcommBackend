10a39682b5b387d23a82025c19f57256c361e998
Sabbir <sabbirrahman42@iut-dhaka.edu>
2021-07-06 23:49:50 +0600

    buy request table data ready for product order

--- controllers/productOrderController.js
+++ controllers/productOrderController.js
@@ -1,3 +1,5 @@
+const User =  require('../model/userModel')
+const userRole = require('../model/userRole')
 const product = require('../model/productModel')
 const productSchema = require('../model/productModel')
 var mongoose = require('mongoose');
@@ -19,19 +21,38 @@ const orderProduct = async(req,res) => {
 
     const product =  await productSchema.findOne({ "_id":productId });
 
+    const user = await User.findOne({ "_id":req.user.id })
+
     if(!product){
-        res.status(400).json({'message':"Product not find"})
+        return res.status(400).json({'message':"Product not find"})
     }
 
     if(quantity< product.minOrder){
-        res.status(400).json({'message':"Please order atleast the minimum amount"}).status(400)
+        return res.status(400).json({'message':"Please order atleast the minimum amount"}).status(400)
     }
 
+    const buyRequest = {
+        "product_id": productId,
+        "buyer_id": req.user.id,
+        "buyerName": req.user?.fullname,
+        "buyerAddress": user?.address,
+        "buyerPhone": user?.phone_no,
+        "buyerEmail": req.user.email,
+        "buyingQuantityUnit": product.unitName,
+        "buyingQuantity": quantity,
+        "buyingMoney": product.unitPrize * quantity,
+        "status" : "pending",
+
+        
+    }
+
+    console.log(buyRequest)
+    
     console.log(product.unitPrize * quantity)
    
     
     
-    res.status(200).json({ 'message': 'Product order Succesfull wait for acceptance'})
+    return res.status(200).json({ 'message': 'Product order Succesfull wait for acceptance'})
 }
 
 module.exports = {testView, orderProduct}
