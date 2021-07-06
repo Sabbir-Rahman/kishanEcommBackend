27112e183d9d38ecd29702fd18e95cc325f1ea25
Sabbir <sabbirrahman42@iut-dhaka.edu>
2021-07-06 20:01:19 +0600

    productOrderRequest and productBuyRequest table created

---
+++ model/productBuyRequestModel.js
@@ -0,0 +1,63 @@
+const mongoose  =  require('mongoose')
+
+//this is for replicating required
+const requiredString = {
+    type: String,
+    required: true
+}
+
+const requiredDate = {
+    type: Date,
+    required: true
+}
+
+const isString = {
+    type: String
+    
+}
+
+const isNumber = {
+    type: Number
+    
+}
+
+const requiredNumber = {
+    type: Number,
+    required: true
+    
+}
+
+const requiredUniqueString = {
+    type: String,
+    required: true,
+    unique: true
+}
+
+const requiredBoolean = {
+    type: Boolean,
+    required: true
+}
+
+
+
+const productBuyRequestSchema = mongoose.Schema({
+    product_id: requiredString,
+    buyer_id: requiredString,
+    buyerName: isString,
+    buyerAddress: isString,
+    buyerPhone: isString,
+    buyerEmail: isString,
+    buyingQuantityUnit: requiredString,
+    buyingQuantity: requiredNumber,
+    buyingMoney: requiredNumber.required,
+    bookingMoney: requiredNumber,
+    status : requiredString,
+    timestamp: {
+        type: Date,
+        default: new Date(),
+    }
+
+    
+})
+
+module.exports = mongoose.model('productBuyRequest', productBuyRequestSchema)
---
+++ model/productOrderRequestModel.js
@@ -0,0 +1,60 @@
+const mongoose  =  require('mongoose')
+
+//this is for replicating required
+const requiredString = {
+    type: String,
+    required: true
+}
+
+const requiredDate = {
+    type: Date,
+    required: true
+}
+
+const isString = {
+    type: String
+    
+}
+
+const isNumber = {
+    type: Number
+    
+}
+
+const requiredNumber = {
+    type: Number,
+    required: true
+    
+}
+
+const requiredUniqueString = {
+    type: String,
+    required: true,
+    unique: true
+}
+
+const requiredBoolean = {
+    type: Boolean,
+    required: true
+}
+
+
+
+const productOrderRequestSchema = mongoose.Schema({
+    product_id: requiredString,
+    seller_id: requiredString,
+    buyingQuantityUnit: requiredString,
+    buyingQuantity: requiredNumber,
+    buyingMoney: requiredNumber.required,
+    bookingMoney: requiredNumber,
+    status : requiredString,
+    timestamp: {
+        type: Date,
+        default: new Date(),
+    }
+
+    
+})
+
+module.exports = mongoose.model('productOrderRequest', productOrderRequestSchema)
+
